67a9f54b91426bde5525332b392d7820390c1fed
Jade Ellis <jade@ellis.link>
2025-02-22 20:43:12 +0000

    Update gomuks.rb

--- Formula/gomuks.rb
+++ Formula/gomuks.rb
@@ -11,7 +11,15 @@ class Gomuks < Formula
   depends_on "libolm"
 
   def install
-    system "./build.sh" #, *std_go_args(ldflags: "-s -w")
+    
+    system "go", "generate", "./web"
+    mautrix_version = `grep 'maunium.net/go/mautrix ' go.mod | head -n1 | awk '{ print $2 }'`.strip
+    ldflags = "-X go.mau.fi/gomuks/version.Tag=#{`git describe --exact-match --tags 2>/dev/null`.strip} " \
+              "-X go.mau.fi/gomuks/version.Commit=#{`git rev-parse HEAD`.strip} " \
+              "-X 'go.mau.fi/gomuks/version.BuildTime=#{Time.now.utc.iso8601}' " \
+              "-X 'maunium.net/go/mautrix.GoModVersion=#{mautrix_version}' " \
+              "-s -w"
+    system "go", "build", *std_go_args(ldflags) "./cmd/gomuks"
   end
 
   test do
